190a6c6c163aadaef1cea7ff888e870d4f753dab
bryanjensen <bryanawjensen@gmail.com>
2015-03-04 04:08:40 +0000

    Updating port for mongo

--- cronjobs/update-data.sh
+++ cronjobs/update-data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/bryanjensen/.bashrc
 PATH=$PATH:/usr/local/bin
-(node compile-players.js && node compile-matches.js && node compile-final.js && node compile-database.js --db_ip localhost:80) > update.log
+(node compile-players.js && node compile-matches.js && node compile-final.js && node compile-database.js) > update.log
