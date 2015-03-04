c848ed967b7c5fd1126fc5fd5b84abc7ba464c56
Bryan Jensen <bryanawjensen@gmail.com>
2015-03-03 23:31:51 -0500

    Checking for file before sourcing

--- cronjobs/update-data.sh
+++ cronjobs/update-data.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-source /home/bryanjensen/.bashrc
+if [ -f /home/bryanjensen/.bashrc ]; then
+    source /home/bryanjensen/.bashrc
+fi
 PATH=$PATH:/usr/local/bin
 (node compile-players.js && node compile-matches.js && node compile-final.js && node compile-database.js) > update.log
