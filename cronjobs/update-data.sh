3f83d73bf3ec9a7ad902dadcdac523841de5f850
bryanjensen <bryanawjensen@gmail.com>
2015-03-13 19:21:07 +0000

    Adjusting cronjob to not automatically pipe to log file

--- cronjobs/update-data.sh
+++ cronjobs/update-data.sh
@@ -3,4 +3,4 @@ if [ -f /home/bryanjensen/.bashrc ]; then
     source /home/bryanjensen/.bashrc
 fi
 PATH=$PATH:/usr/local/bin
-(node compile-players.js && node compile-matches.js && node compile-final.js && node compile-database.js) > update.log
+node compile-players.js && node compile-matches.js && node compile-final.js && node compile-database.js
